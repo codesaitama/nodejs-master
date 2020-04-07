90a0a9cb8b14945b4fe8222dfadc2fe200ee15a9
Achana Emm Jnr <achanamosey@gmail.com>
2020-04-07 08:41:15 +0100

    Made modifications to the project. Added the [checks] functionality. Finished on the handler.check.post function.

--- index.js
+++ index.js
@@ -102,5 +102,7 @@ let unifiedServer = function(req, res) {
 // Define request router.
 let router = {
     ping: handlers.ping,
-    users: handlers.users
+    users: handlers.users,
+    tokens: handlers.tokens,
+    checks: handlers.checks
 }
